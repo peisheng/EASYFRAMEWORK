b8b36db15f64935123d756fa036148d417a1dad2
peisheng <ilovemeyou2000@163.com>
2014-02-20 12:33:17 +0800

    update..

---
+++ ReplaceTool/ReplaceTool/Hepler/ControlHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+using System.Windows;
+using System.Windows.Media;
+
+
+namespace ReplaceTool.Hepler
+{
+    public static class ControlsHelper
+    {
+
+        public static List<Visual> EnumVisual(Visual myVisual, ref List<Visual> EnumVisualList)
+        {
+            if (EnumVisualList == null) {
+                EnumVisualList = new List<Visual>();
+            }
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(myVisual); i++)
+            {
+                Visual childVisual = (Visual)VisualTreeHelper.GetChild(myVisual, i);
+                if (childVisual != null)
+                {
+                    EnumVisualList.Add(childVisual);
+                    EnumVisual(childVisual,ref EnumVisualList);
+                    
+                }
+            }
+            return EnumVisualList;
+        }
+
+       
+        /// <summary>
+        /// 获取DependencyObject的所以子元素的扩展
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static IEnumerable<DependencyObject> GetVisuals(this DependencyObject root)
+        {
+            int count = VisualTreeHelper.GetChildrenCount(root);
+            for (int i = 0; i < count; i++)
+            {
+                var child = VisualTreeHelper.GetChild(root, i);
+                yield return child;
+                foreach (var descendants in child.GetVisuals())
+                {
+                    yield return descendants;
+                }
+            }
+        }
+    }
+}
